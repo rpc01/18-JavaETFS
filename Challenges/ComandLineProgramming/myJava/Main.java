c3c6a089428591fed949dc82b00e31870d88c868
rpc01 <rpratc@gmail.com>
2018-06-20 11:26:52 +0200

    FirstChallenge

---
+++ Challenges/ComandLineProgramming/myJava/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter user name: ");
+    String name = sc.nextLine();
+
+    System.out.print("Enter user age: ");
+
+    int age = sc.nextInt();
+
+    System.out.print("Your Name is " +name );
+    System.out.print(" and your Age is " +age +".");
+    sc.close();
+  }
+}
